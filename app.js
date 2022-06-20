92b0474f907e22ed8b3fb4071aed3732bf71f30b
Stephen Polley <stephenHpolley@gmail.com>
2022-06-20 14:39:54 +0000

    Changes to travis

--- app.js
+++ app.js
@@ -2,3 +2,14 @@ const express = require("express")
 
 
 const app = express()
+
+
+app.get( "/", (req, res) => {
+    res.send("Home")
+})
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () =>{
+    console.log(`Listening on http://localhost:${PORT}`)
+})
